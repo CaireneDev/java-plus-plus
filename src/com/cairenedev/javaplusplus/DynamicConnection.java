8d8da90940702174e74ff2e61ae0b08ceb8ae781
Mohamed Osama <mohamed.o.alnagdy@gmail.com>
2015-04-26 19:53:59 +0300

    Finalized DynamicConnection.java

--- src/com/cairenedev/javaplusplus/DynamicConnection.java
+++ src/com/cairenedev/javaplusplus/DynamicConnection.java
@@ -1,10 +1,48 @@
 package com.cairenedev.javaplusplus;
 
+/**
+ *  The <tt>DynamicConnection</tt> class represents a union-find data structure.
+ *  <p>
+ *  This implementation uses weighted quick union by size (without path compression).
+ *  
+ *  <em>Initialization (size n)</em>
+ *  ** Linear (n)
+ *  
+ *  <em>reset()</em>
+ *  ** Linear (n)
+ *  
+ *  <em>resize(int n)</em>
+ *  ** Worst case Linear (n)
+ *  
+ *  <em>resize(int n, boolean keep)</em>
+ *  ** Worst case Linear (n)
+ *  
+ *  <em>union(int first_element, int second_element)</em>
+ *  **  Worst case log(n)
+ *  
+ *  <em>find(int element)</em>
+ *  **  Worst case log(n)
+ *  
+ *  <em>connected(int first_element, second_element)</em>
+ *  **  Worst case log(n)
+ *  
+ *  <em>connected(int[] elements)</em>
+ *  **  Worst case n.log(n)
+ *  
+ *  <em>size()</em>
+ *  ** constant time (returns original components count)
+ *  <em>count()</em>
+ *  ** constant time (returns current components count)
+ *     
+ *  @author Mohamed Osama <mohamed@cairenedev.com>
+ */
+
 public class DynamicConnection {
 	
 	private int[] parents_array;
 	private int[] sizes_array;
 	private int size;
+	private int componentsCount;
 	
 	public DynamicConnection(int n) {
 		resize(n);
@@ -15,10 +53,11 @@ public class DynamicConnection {
 	}
 	
 	public int count() {
-		return size;
+		return componentsCount;
 	}
 	
 	public int find(int element) {
+		if (element < 0 || element >= size) return -1;
 		while(element != parents_array[element]) element = parents_array[element];
 		return element;
 	}
@@ -34,10 +73,26 @@ public class DynamicConnection {
 		return true;
 	}
 	
+    public void union(int first_element, int second_element) {
+        int first_root = find(first_element);
+        int second_root = find(second_element);
+        if (first_root == second_root) return;
+        if (sizes_array[first_root] < sizes_array[second_root]) {
+            parents_array[first_root] = second_root;
+            sizes_array[second_root] += sizes_array[first_root];
+        }
+        else {
+            parents_array[second_root] = first_root;
+            sizes_array[first_root] += sizes_array[second_root];
+        }
+        componentsCount--;
+    }
+	
 	public void reset() {
 		for(int i = 0; i < size; i++) {
 			parents_array[i] = i;
 			sizes_array[i] = 1;
+			this.componentsCount = size;
 		}
 	}
 	
@@ -64,6 +119,7 @@ public class DynamicConnection {
 				else {
 					parents[i] = i;
 					sizes[i] = 1;
+					componentsCount++;
 				}
 			}
 			initializeArrays(parents, sizes);
